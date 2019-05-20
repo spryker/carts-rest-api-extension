cfde003ac98fa5b36ece6e2600aaa0b3832268ec
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-05-20 20:17:33 +0300

    GLUE-4983 injected stack of quote collection expander plugins into the CartsRestApi.

---
+++ src/Spryker/Zed/CartsRestApiExtension/Dependency/Plugin/QuoteCollectionExpanderPluginInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CartsRestApiExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\CustomerTransfer;
+use Generated\Shared\Transfer\QuoteCollectionTransfer;
+
+interface QuoteCollectionExpanderPluginInterface
+{
+    /**
+     * Specification:
+     * - Expands quote collection transfer with additional data.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CustomerTransfer $customerTransfer
+     * @param \Generated\Shared\Transfer\QuoteCollectionTransfer $quoteCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteCollectionTransfer
+     */
+    public function expandQuoteCollection(
+        CustomerTransfer $customerTransfer,
+        QuoteCollectionTransfer $quoteCollectionTransfer
+    ): QuoteCollectionTransfer;
+}
